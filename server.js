afa8ad8100d6f60fa620fe6dd881a5798b49d997
Abu <abubakar.saad.891@gmail.com>
2016-02-22 15:52:44 -0500

    Add Delete todo routes

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/todos/:id', function(req, res){
    //});
 
     if(matchedTodo){
-        res.json(matchedTodo);
+        res.json(matchedTodo);n
     }else{
         res.status(404).send();
     }
@@ -62,6 +62,19 @@ app.post('/todos',function(req, res){
 });
 
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if(matchedTodo){
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }else{
+        res.status(404).json({"error": "no todo found with that id"});
+    }
+
+});
 
 app.listen(PORT, function(){
     console.log('Express listening on port ' + PORT + '!');
