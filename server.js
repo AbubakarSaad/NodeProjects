0bd92e83f0f70867cb3a33fe0b203037c024d965
Abu <abubakar.saad.891@gmail.com>
2016-02-22 18:58:52 -0500

    Add Put /todos/:id

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/todos/:id', function(req, res){
    //});
 
     if(matchedTodo){
-        res.json(matchedTodo);n
+        res.json(matchedTodo);
     }else{
         res.status(404).send();
     }
@@ -66,7 +66,7 @@ app.post('/todos',function(req, res){
 app.delete('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
-    
+
     if(matchedTodo){
         todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
@@ -74,6 +74,41 @@ app.delete('/todos/:id', function(req, res){
         res.status(404).json({"error": "no todo found with that id"});
     }
 
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req,res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if(!matchedTodo){
+        return res.status(404).send();
+    }
+
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    }else if(body.hasOwnProperty('completed')){
+        // Bad
+        return res.status(400).send();
+    }else {
+        // Never provided attribute, no problem here
+    }
+
+    // description
+    if(body.hasOwnProperty('description') && _.isString(body.completed) && body.description.trim().length > 0){
+        validAttributes.description = body.description;
+    }else if(body.hasOwnProperty('description')){
+        // Bad
+        return res.status(400).send();
+    }else {
+        // Never provided attribute, no problem here
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
 });
 
 app.listen(PORT, function(){
