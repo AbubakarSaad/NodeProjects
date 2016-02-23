a9bfeea64dda45d2e45cb24285c4cc8387af1ae7
Abu <abubakar.saad.891@gmail.com>
2016-02-22 21:59:06 -0500

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+/**
+ * Created by abubakarsaad on 2016-02-22.
+ */
+//var person = {
+//    name: 'Abu',
+//    age: 24
+//};
+//
+//function updatePerson(obj){
+//    //obj = {
+//    //    name: 'Abu',
+//    //    age: 25
+//    //};
+//    obj.age = 26;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+
+// Array Example
+var arr = [15, 20];
+
+function updateArray(arr, val){
+    //arr.push(val);
+    arr = [25];
+    debugger;
+}
+
+updateArray(arr, 25);
+console.log(arr);
