eed3af42c3f8678e17567f196b414a96e4699034
Abu <abubakar.saad.891@gmail.com>
2016-03-08 01:18:39 -0500

    Convert GET /todo/:id to use sequelize

--- server.js
+++ server.js
@@ -44,29 +44,23 @@ app.get('/todos',function(req, res){
 // GET /todos/:id
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-   // var matchTodo;
-   // // Itearte of todos array. Find the match.
-   //todos.forEach(function(todo){
-   //   if(todoId === todo.id){
-   //       matchTodo = todo;
-   //   }
-   //});
 
-    if(matchedTodo){
-        res.json(matchedTodo);
-    }else{
-        res.status(404).send();
-    }
+
+    db.todo.findById(todoId).then(function(todo){
+        if(!!todo){
+            res.json(todo.toJSON());
+        }else{
+            res.status(404).send();
+        }
+    }, function(e){
+        res.status(500).send();
+    });
+
 });
 
 // POST /todos
 app.post('/todos',function(req, res){
     var body = _.pick(req.body, 'description', 'completed'); // user _.pick to only pick description and completed
-
-    // call create on db.todo
-    //  respond with 200 and todo object .toJSON
-    //   fail res.status(400).json(e)
 
 
     db.todo.create(body).then(function(todo){
@@ -74,21 +68,6 @@ app.post('/todos',function(req, res){
     }, function(e){
         res.status(400).json(e);
     });
-
-    //if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-    //   return res.status(400).send();
-    //}
-    //
-    //// set body.description to be trimmed value
-    //
-    //body.description = body.description.trim();
-    //// add id field
-    //body.id = todoNextId;
-    //// push body into array
-    //todos.push(body);
-    //todoNextId++;
-    //
-    //res.json(body);
 });
 
 
